152850904f7c3a701aad702beee655bbaf57a329
apljee <apljee23@hotmail.com>
2014-01-09 16:59:17 -0500

    Create source.js

---
+++ source/source.js
@@ -0,0 +1,194 @@
+var NewExpansionMod = {};
+(function(){
+GDT.addResearchItem({
+	id: "05f5328e-5a70-42d1-9f2a-2ff7814d3674",
+	name: "Holographic Graphics V1",
+	canResearch: function () {
+						var res = true;
+						return res;
+				},
+	category: "Engine",
+	categoryDisplayName: "Engine",
+	v: 1
+	});
+GDT.addResearchItem({
+	id: "b79cf118-73fc-4f7f-9b4f-621602afd98a",
+	name: "Holographic Graphics V2",
+	canResearch: function () {
+						var res = true;
+						return res;
+				},
+	category: "Engine",
+	categoryDisplayName: "Engine",
+	v: 1
+	});
+GDT.addResearchItem({
+	id: "74f01815-84fd-4ddd-8403-2557d3fb17fb",
+	name: "Holographic Graphics V3",
+	canResearch: function () {
+						var res = true;
+						return res;
+				},
+	category: "Engine",
+	categoryDisplayName: "Engine",
+	v: 1
+	});
+GDT.addResearchItem({
+	id: "5b0bc3cd-197b-4488-bd46-0a5c39ce463c",
+	name: "Holographic Graphics V4",
+	canResearch: function () {
+						var res = true;
+						return res;
+				},
+	category: "Engine",
+	categoryDisplayName: "Engine",
+	v: 1
+	});
+GDT.addResearchItem({
+	id: "b0f98487-3f7e-4ed2-abbd-702542f93494",
+	name: "Holographic Graphics V5",
+	canResearch: function () {
+						var res = true;
+						return res;
+				},
+	category: "Engine",
+	categoryDisplayName: "Engine",
+	v: 1
+	});
+GDT.addResearchItem({
+	id: "30dc82c0-1cca-40c6-b0a0-4791b660321f",
+	name: "HoloAI V1",
+	canResearch: function () {
+						var res = true;
+						return res;
+				},
+	category: "Engine",
+	categoryDisplayName: "Engine",
+	v: 1
+	});
+GDT.addResearchItem({
+	id: "5808024c-b031-4650-b1f6-426cc237a0f2",
+	name: "HoloAI V2",
+	canResearch: function () {
+						var res = true;
+						return res;
+				},
+	category: "Engine",
+	categoryDisplayName: "Engine",
+	v: 1
+	});
+GDT.addResearchItem({
+	id: "53352e14-5eca-4da9-89e8-83ea78bad822",
+	name: "HoloAI V3",
+	canResearch: function () {
+						var res = true;
+						return res;
+				},
+	category: "Engine",
+	categoryDisplayName: "Engine",
+	v: 1
+	});
+GDT.addResearchItem({
+	id: "f40ec304-153b-4124-8cad-0873201ba94c",
+	name: "HoloAI V4",
+	canResearch: function () {
+						var res = true;
+						return res;
+				},
+	category: "Engine",
+	categoryDisplayName: "Engine",
+	v: 1
+	});
+GDT.addResearchItem({
+	id: "88afb68e-a545-44ec-81a8-5572a5b4909e",
+	name: "HoloAI V5",
+	canResearch: function () {
+						var res = true;
+						return res;
+				},
+	category: "Engine",
+	categoryDisplayName: "Engine",
+	v: 1
+	});
+GDT.addTopics([
+{ 
+	id: "4004dbb1-225c-416b-90ea-719d530a3e85",
+	name: "Tycoon",
+	genreWeightings: [  0.7, 0.7, 0.7, 0.9, 1, 1 ],
+	audienceWeightings: [  0.9, 0.9, 0.9 ]
+	 }
+,{ 
+	id: "b630bef8-0764-489e-832a-14303f86d7d0",
+	name: "Virtual Tabletop",
+	genreWeightings: [  0.9, 1, 1, 0.8, 0.7, 0.8 ],
+	audienceWeightings: [  0.7, 0.8, 0.9 ]
+	 }
+,{ 
+	id: "ceaed6bb-50d5-4f2f-9858-b098e5c284ee",
+	name: "Prison",
+	genreWeightings: [  1, 1, 1, 0.9, 0.7, 0.7 ],
+	audienceWeightings: [  0.6, 0.7, 1 ]
+	 }
+,{ 
+	id: "e0ab2f5f-ff76-4361-9c34-b5b2c75b93eb",
+	name: "Tower Defence",
+	genreWeightings: [  0.7, 0.6, 0.6, 0.9, 1, 1 ],
+	audienceWeightings: [  0.8, 1, 0.8 ]
+	 }
+,{ 
+	id: "176837fa-b243-4737-a938-29e56436f32e",
+	name: "Hit and Run",
+	genreWeightings: [  1, 0.8, 0.7, 0.6, 0.6, 1 ],
+	audienceWeightings: [  0.7, 0.8, 1 ]
+	 }
+,{ 
+	id: "1add1a44-47a7-4989-8f96-dd3efee1b6aa",
+	name: "Hitman",
+	genreWeightings: [  1, 0.8, 0.9, 0.7, 0.6, 0.7 ],
+	audienceWeightings: [  0.6, 0.7, 1 ]
+	 }
+,{ 
+	id: "56628fb2-b13f-46bf-8895-5a88bddea293",
+	name: "Theft",
+	genreWeightings: [  1, 1, 1, 0.6, 0.6, 0.8 ],
+	audienceWeightings: [  0.7, 0.9, 1 ]
+	 }
+,{ 
+	id: "d8c799f6-bd09-45b8-968e-20901327409d",
+	name: "Farming",
+	genreWeightings: [  0.6, 0.6, 0.8, 1, 0.9, 1 ],
+	audienceWeightings: [  1, 1, 0.8 ]
+	 }
+,{ 
+	id: "1fc50d26-cf60-4d5c-999a-d8b6d1a5863f",
+	name: "Turn Based Strategy",
+	genreWeightings: [  0.6, 0.6, 0.8, 1, 1, 0.9 ],
+	audienceWeightings: [  0.7, 0.8, 0.9 ]
+	 }
+,{ 
+	id: "63f6f0fc-0027-4a04-92b1-57075c27338d",
+	name: "Real Time Strategy",
+	genreWeightings: [  0.8, 0.7, 0.6, 1, 1, 0.7 ],
+	audienceWeightings: [  0.6, 0.7, 1 ]
+	 }
+,{ 
+	id: "c583b386-4865-4494-ade8-3f855f91ba1f",
+	name: "Management",
+	genreWeightings: [  0.6, 0.7, 0.8, 1, 0.9, 0.9 ],
+	audienceWeightings: [  0.9, 0.9, 0.8 ]
+	 }
+,{ 
+	id: "7d5e4f80-32e5-41a2-a223-caccd2c934fe",
+	name: "Cooking",
+	genreWeightings: [  0.6, 0.7, 0.6, 1, 0.8, 1 ],
+	audienceWeightings: [  1, 0.9, 0.6 ]
+	 }
+,{ 
+	id: "b35ae32e-e4e2-4990-b7fd-d5982d3d8cd8",
+	name: "Airplane Sim",
+	genreWeightings: [  0.8, 0.8, 0.7, 1, 0.9, 0.9 ],
+	audienceWeightings: [  0.8, 0.8, 0.8 ]
+	 }
+
+]);
+})();
